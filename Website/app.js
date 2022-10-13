2b84ba12b682136475af266df3bba18f419ca212
Your Name <agandhi29@myseneca.ca>
2022-10-12 22:01:29 -0400

    Fixed spelling error on website

--- Website/app.js
+++ Website/app.js
@@ -4,21 +4,22 @@ const defaultTheme = localStorage.getItem('theme') || 'theme-dark';
 setTheme(defaultTheme);
 
 themeSwitcher.addEventListener('change', (e) => {
-    setTheme(e.target.value);
-})
+	setTheme(e.target.value);
+});
 
 function setTheme(theme) {
-    theme = theme || 'theme-dark';
-    document.documentElement.className = theme;
-    localStorage.setItem('theme', theme);
-    themeSwitcher.value = theme;
+	theme = theme || 'theme-dark';
+	document.documentElement.className = theme;
+	localStorage.setItem('theme', theme);
+	themeSwitcher.value = theme;
 }
 
-const componentBoxes = document.querySelectorAll('.main__html-componentBox')
+const componentBoxes = document.querySelectorAll('.main__html-componentBox');
 
-componentBoxes.forEach(componentBox => {
-    componentBox.addEventListener('click', () => {
-        let a = componentBox.lastElementChild.lastElementChild.firstElementChild
-        location.href = a.href
-    })
-})
+componentBoxes.forEach((componentBox) => {
+	componentBox.addEventListener('click', () => {
+		let a =
+			componentBox.lastElementChild.lastElementChild.firstElementChild;
+		location.href = a.href;
+	});
+});
